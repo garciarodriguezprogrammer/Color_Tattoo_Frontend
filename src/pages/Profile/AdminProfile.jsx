7c0b0222e0ee3b906265ef89a90659f542be5fab
Dayanis <dayany1588@yahoo.com>
2024-03-12 00:10:55 +0100

    Cada usuario ya tiene acceso a los datos de su perfil, una vez acceda a su área personal

--- src/pages/Profile/AdminProfile.jsx
+++ src/pages/Profile/AdminProfile.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from "react";
 import { GetProfileData } from "../../services/apiCalls";
 
 
-
 export const AdminProfile = () => {
 
     const [datos, setDatos] = useState(null)
